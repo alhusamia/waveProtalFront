6f8f3ad6317f089cd82392111f7ad74e4c7359f9
Yazan Alhusamia <sadprincegost@gmail.com>
2022-01-03 15:25:11 +0200

    second push

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ccb215823628cd4"; }
+/******/ 		__webpack_require__.h = function() { return "1bb490d873139f15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
